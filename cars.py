0288783ab9a64ad84e7c39bc7a8ffcf621473020
MerMer223 <mehraabjhajj123@icloud.com>
2025-03-16 11:10:48 -0400

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,7 +1,7 @@
 class Cars():
     def __init__(name,model,brand,speed):
-        name.model = name
-        name.brand = name
+        name.model = model
+        name.brand = brand
         name.speed = speed
     def printdetails(name):
         print(name.model,name.brand,name.speed)
@@ -9,4 +9,8 @@ class Cars():
         print(1,2,3)
         choice = int(input("type 1 to accelerate or decelerate"))
         if choice == 1:
-            name.model = input("Enter new speed")
+            name.speed = input("Enter new speed")
+
+
+c1 = Cars(2024,"Tesla",100)
+c1.printdetails()

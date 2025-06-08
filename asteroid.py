2f6087ae36257c4b1b261a5d01c66f582f115554
MerMer223 <mehraabjhajj123@icloud.com>
2025-06-08 10:59:06 -0400

    Add files via upload

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import math
+import random
 pygame.init()
 WIDTH = 800
 HEIGHT = 800
@@ -20,6 +21,8 @@ def Redraw_gamewindow():
     player.draw(screen)
     for b in player_bullets:
         b.draw(screen)
+    for a in asteroids:
+        a.draw(screen)
     pygame.display.update()
 class Player():
     def __init__(self):
@@ -34,7 +37,7 @@ class Player():
         self.rotatedrect.center = (self.x,self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine * self.w // 2, self.y + self.sine * self.h // 2)
+        self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
     def turn_left(self):
         self.angle += 5
         self.rotatedsurface = pygame.transform.rotate(self.image,self.angle)
@@ -42,7 +45,7 @@ class Player():
         self.rotatedrect.center = (self.x,self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine * self.w // 2, self.y + self.sine * self.h // 2)
+        self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
     def turn_right(self):
         self.angle -= 5
         self.rotatedsurface = pygame.transform.rotate(self.image,self.angle)
@@ -50,7 +53,7 @@ class Player():
         self.rotatedrect.center = (self.x,self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine * self.w // 2, self.y + self.sine * self.h // 2)
+        self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
     def forward(self):
         self.x += self.cosine * 6
         self.y -= self.sine * 6
@@ -59,10 +62,19 @@ class Player():
         self.rotatedrect.center = (self.x,self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine * self.w // 2, self.y + self.sine * self.h // 2)
+        self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
     def draw(self,screen):
         #screen.blit(self.image,[self.x,self.y,self.w,self.h])
         screen.blit(self.rotatedsurface,self.rotatedrect)
+    def updateLocation(self):
+        if self.x > WIDTH + 50:
+            self.x = 0
+        elif self.x < 0 -self.w:
+            self.x = WIDTH
+        elif self.y < - 50:
+            self.y = HEIGHT
+        elif self.y > HEIGHT + 50:
+            self.y = 0
 
 class Bullet():
     def __init__(self):
@@ -74,12 +86,42 @@ class Bullet():
         self.s = player.sine
         self.xv = self.c * 10
         self.yv = self.s * 10
-
     def move(self):
         self.x += self.xv
         self.y -= self.yv
     def draw(self,screen):
         pygame.draw.rect(screen,(255,255,255),[self.x,self.y,self.w,self.h])
+    def checkOffScreen(self):
+        if self.x < -50 or self.x > WIDTH or self.y > HEIGHT or self.y < -50: 
+            return True
+class Asteroid():
+    def __init__(self,rank):
+        self.rank = rank
+        if self.rank == 1:
+            self.image = Sasteroid
+        elif self.rank == 2:
+            self.image = Masteroid
+        else:
+            self.image = Lasteroid
+        self.w = 50 * rank
+        self.h = 50 * rank
+        self.ranPoint = random.choice([(random.randrange(0,WIDTH - self.w),random.choice([-1*self.h-5,HEIGHT + 5])),(random.choice([-1*self.w - 5, WIDTH + 5]),random.randrange(0, HEIGHT - self.h))])
+        self.x,self.y = self.ranPoint
+        if self.x < WIDTH//2:
+            self.xdir = 1
+        else:
+            self.xdir = -1
+        if self. y < HEIGHT // 2:
+            self.ydir =1
+        else: self.ydir = -1
+        self.xv = self.xdir *random.randrange(1,3)
+        self.yv = self.ydir *random.randrange(1,3)
+    def draw(self, screen):
+        screen.blit(self.image,(self.x,self.y))
+asteroids = []
+count = 0
+
+
 
 
 player_bullets = []
@@ -87,7 +129,44 @@ player = Player()
 run = True
 while run:
     clock.tick(60)
+    count += 1
     if not GameOver:
+        if count % 50 == 0:
+            ran = random.choice([1,1,1,2,2,3])
+            asteroids.append(Asteroid(ran))
+        player.updateLocation()
+        for b in player_bullets:
+            b.move()
+            if b.checkOffScreen():
+                player_bullets.pop(player_bullets.index(b))
+        for a in asteroids:
+            a.x += a.xv
+            a.y += a.yv
+        
+            for b in player_bullets:
+                if (b.x >= a.x and b.x <= a.x + a.w) or b.x + b.w >= a.x and b.x +b.w <= a.x +a.w:
+                    if (b.y >= a.y and b.y <= a.y +a.h) or b.y >= b.h >= a.y and b.y + b.h <= a.y + a.h:
+                        if a.rank == 3:
+                            na1 = Asteroid(2)
+                            na2 = Asteroid(2)
+                            na1.x = a.x
+                            na2.x = a.x
+                            na1.y =a.y
+                            na2.y = a.y
+                            asteroids.append(na1)
+                            asteroids.append(na2)
+                        elif a.rank == 2:
+                            na1 = Asteroid(1)
+                            na2 = Asteroid(1)
+                            na1.x = a.x
+                            na2.x = a.x
+                            na1.y =a.y
+                            na2.y = a.y
+                            asteroids.append(na1)
+                            asteroids.append(na2)
+                        asteroids.pop(asteroids.index(a))
+                        player_bullets.pop(player_bullets.index(b))
+                        
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             player.turn_left()
